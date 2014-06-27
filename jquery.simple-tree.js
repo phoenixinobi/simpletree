5a4c5da881036ae1141b2f5c04399040e93991ed
phoenixinobi <phoenixinobi@gmail.com>
2014-06-28 00:21:12 +0800

    Update jquery.simple-tree.js

--- jquery.simple-tree.js
+++ jquery.simple-tree.js
@@ -4,9 +4,10 @@
  * (c) phoenixinobi - 2014.
  * https://github.com/phoenixinobi/simpletree/
  */
- 
+
 function InitTree(tree) {
     $(tree).each(function () {
+        $(tree).parents("ul").css("padding-left", "20px").css("padding-top", "2px").css("padding-bottom", "2px");
         $(this).children(".tree-branch").each(function () {
             InitBranch(this);
         });
@@ -16,9 +17,25 @@ function InitTree(tree) {
     });
 }
 
+function CollapseBranch(branch) {
+    $(branch).children("img").first().remove();
+    AttachExpandCommandImage(branch);
+    $(branch).parent().children("ul").each(function () {
+        $(this).hide();
+    });
+}
+
+function ExpandBranch(branch) {
+    $(branch).children("img").first().remove();
+    AttachCollapseCommandImage(branch);
+    $(branch).parent().children("ul").each(function () {
+        $(this).show();
+    });
+}
+
 function InitBranch(branch) {
     AttachSpacerImage(branch);
-    AttachCollapseCommandImage(branch);
+    CollapseBranch(branch);
     BindBranchClick(branch);
     $(branch).css("cursor", "pointer");
 }
@@ -27,18 +44,10 @@ function BindBranchClick(branch) {
     $(branch).click(function () {
         var command = $(this).children("img").first().attr("tree-command");
         if (command == "collapse") {
-            $(this).children("img").first().remove();
-            AttachExpandCommandImage(branch);
-            $(this).parent().children("ul").each(function () {
-                $(this).hide();
-            });
+            CollapseBranch(this);
         }
         else if (command == "expand") {
-            $(this).children("img").first().remove();
-            AttachCollapseCommandImage(branch);
-            $(this).parent().children("ul").each(function () {
-                $(this).show();
-            });
+            ExpandBranch(this);
         }
     });
 }
@@ -48,44 +57,26 @@ function InitLeaf(leaf) {
     AttachLeafImage(leaf);
 }
 
-function BindLeafClick(leaf) {
-    $(leaf).click(function () {
-
-        if ($(this).text().indexOf("-") == 0) {
-            $(this).text($(this).text().replace("-", "+"));
-            $(this).parent().children("ul").each(function () {
-                $(this).hide();
-            });
-        }
-        else if ($(this).text().indexOf("+") == 0) {
-            $(this).text($(this).text().replace("+", "-"));
-            $(this).parent().children("ul").each(function () {
-                $(this).show();
-            });
-        }
-    });
-}
-
 function AttachCollapseCommandImage(branch) {
-    $('<img src="@Url.Content("~/content/images/remove.png")">').load(function () {
+    $('<img src="../content/images/remove.png">').load(function () {
         $(branch).prepend($(this).width(12).height(12).attr("tree-command", "collapse"));
     });
 }
 
 function AttachExpandCommandImage(branch) {
-    $('<img src="@Url.Content("~/content/images/add_new.png")">').load(function () {
+    $('<img src="../content/images/add_new.png">').load(function () {
         $(branch).prepend($(this).width(12).height(12).attr("tree-command", "expand"));
     });
 }
 
 function AttachLeafImage(leaf) {
-    $('<img src="@Url.Content("~/content/images/report.png")">').load(function () {
-        $(leaf).prepend($(this).width(24).height(24));
+    $('<img src="../content/images/report.png">').load(function () {
+        $(leaf).prepend($(this).width(20).height(16));
     });
 }
 
 function AttachSpacerImage(node) {
-    $('<img src="@Url.Content("~/content/images/spacer.png")">').load(function () {
-        $(node).prepend($(this).width(12).height(24));
+    $('<img src="../content/images/spacer.png">').load(function () {
+        $(node).prepend($(this).width(10).height(16));
     });
 }
